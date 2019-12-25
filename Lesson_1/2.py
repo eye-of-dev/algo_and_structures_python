82e2ac7c323c4c3d581ca776f0927e28d82f456c
Anton Lisikov <anton.lisikov@gmail.com>
2019-12-25 22:32:20 +0600

    Сделаны задания к первому уроку

--- Lesson_1/2.py
+++ Lesson_1/2.py
@@ -1,3 +1,17 @@
-# 2. Выполнить логические побитовые операции "И", "ИЛИ" и др.
-# над числами 5 и 6. Выполнить
-# над числом 5 побитовый сдвиг вправо и влево на два знака.
+"""
+# 2. Выполнить логические побитовые операции "И", "ИЛИ" и др. над числами 5 и 6.
+# Выполнить над числом 5 побитовый сдвиг вправо и влево на два знака.
+"""
+
+NUMB_FIVE = 5
+NUMB_SIX = 6
+
+print("%d & %d = %d (%s)" % (NUMB_FIVE, NUMB_SIX, NUMB_FIVE & NUMB_SIX,
+                             bin(NUMB_FIVE & NUMB_SIX)))
+print("%d | %d = %d (%s)" % (NUMB_FIVE, NUMB_SIX, NUMB_FIVE | NUMB_SIX,
+                             bin(NUMB_FIVE | NUMB_SIX)))
+print("%d ^ %d = %d (%s)" % (NUMB_FIVE, NUMB_SIX, NUMB_FIVE ^ NUMB_SIX,
+                             bin(NUMB_FIVE ^ NUMB_SIX)))
+
+print("%d << 2 = %d (%s)" % (NUMB_FIVE, NUMB_FIVE << 2, bin(NUMB_FIVE << 2)))
+print("%d >> 2 = %d (%s)" % (NUMB_FIVE, NUMB_FIVE >> 2, bin(NUMB_FIVE >> 2)))
--- Lesson_1/9.py
+++ Lesson_1/9.py
@@ -1,3 +1,14 @@
+"""
 # 9.Вводятся три разных числа. Найти, какое из них
 # является средним (больше одного, но меньше другого).
+"""
+NUMB_A = int(input('Первое число: '))
+NUMB_B = int(input('Второе число: '))
+NUMB_C = int(input('Третье число: '))
 
+if NUMB_B < NUMB_A < NUMB_C or NUMB_C < NUMB_A < NUMB_B:
+    print('Среднее:', NUMB_A)
+elif NUMB_A < NUMB_B < NUMB_C or NUMB_C < NUMB_B < NUMB_A:
+    print('Среднее:', NUMB_B)
+else:
+    print('Среднее:', NUMB_C)
