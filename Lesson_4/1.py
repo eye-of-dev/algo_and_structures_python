046b8a5d68cba8c9d091b1329d49f757e9aadfd7
Anton Lisikov <anton.lisikov@gmail.com>
2020-01-19 23:53:12 +0600

    feat: сделаны задания к четвертому уроку

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,83 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+from random import randint
+from random import shuffle
+from timeit import Timer
+
+
+def change_places_min_max():
+    """
+    Функция меняет местами минимальный и
+    максимальный элементы в массиве
+    :return: Массив
+    """
+
+    my_list = list(set(randint(0, 30) for i in range(30)))
+    # определим переменные, минимальному зададим максимальное значение,
+    # а максимальному минимальное
+    MIN_EL = 30
+    MAX_EL = 0
+    MIN_EL_KEY = 0
+    MAX_EL_KEY = 0
+
+    # перемешаем "массив", чтобы было интереснее
+    shuffle(my_list)
+    for key, value in enumerate(my_list):
+
+        # поиск максимального и минимального
+        # элементов и запоминаем их индексы
+        # min() и max() неинтересны
+        if value > MAX_EL:
+            MAX_EL = value
+            MAX_EL_KEY = key
+        elif value < MIN_EL:
+            MIN_EL = value
+            MIN_EL_KEY = key
+
+        # при случае, когда переменная MAX_EL равна максимальному значению
+        # а MIN_EL минимальному - прекращаем поиск
+        if MAX_EL == 30 and MIN_EL == 0:
+            break
+    # переставляем местами минимальное и максимальное значение
+    my_list[MIN_EL_KEY], my_list[MAX_EL_KEY] = my_list[MAX_EL_KEY], my_list[MIN_EL_KEY]
+    return my_list
+
+
+t1 = Timer("change_places_min_max()", setup="from __main__ import change_places_min_max")
+print('change_places_min_max', t1.timeit(number=1000), 'milliseconds')
+
+
+def change_places_min_max_two():
+    """
+    Функция меняет местами минимальный и
+    максимальный элементы в массиве
+    :return: Массив
+    """
+
+    my_list = list(set(randint(0, 30) for i in range(30)))
+    # определим переменные, минимальному зададим максимальное значение,
+    # а максимальному минимальное
+
+    MIN_EL = min(my_list)
+    MAX_EL = max(my_list)
+
+    shuffle(my_list)
+
+    MIN_EL_KEY = my_list.index(MIN_EL)
+    MAX_EL_KEY = my_list.index(MAX_EL)
+
+    # переставляем местами минимальное и максимальное значение
+    my_list[MIN_EL_KEY], my_list[MAX_EL_KEY] = my_list[MAX_EL_KEY], my_list[MIN_EL_KEY]
+    return my_list
+
+
+t2 = Timer("change_places_min_max_two()", setup="from __main__ import change_places_min_max_two")
+print('change_places_min_max_two', t2.timeit(number=1000), 'milliseconds')
+
+# Сложность похожа на линейную
+# timeit на первую реализацию выдает результат change_places_min_max 0.07574980199933634 milliseconds
+# timeit на вторую реализацию выдает результат change_places_min_max_two 0.07802550199994585 milliseconds
+# Разница не в пользу стандартных функций, скорее всего
+# из-за двойного поиска, т.е. сперва ищем элемент, а потом еще и индекс
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,89 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+from timeit import Timer
+
+
+def search_prime(numb):
+    """
+    нахождения i-го по счёту простого числа
+    :param numb: Порядковый номер простого числа
+    :return: Возвращает число
+    """
+    count, number, prime = 1, 1, [2]
+
+    if numb == 1:
+        return 2
+
+    while count != numb:
+        number += 2
+
+        for num in prime:
+            if number % num == 0:
+                break
+        else:
+            count += 1
+            prime.append(number)
+
+    return number
+
+
+t1 = Timer("search_prime(5)", setup="from __main__ import search_prime")
+print('search_prime', t1.timeit(number=1000), 'milliseconds')
+
+
+# Сложность близка к O(n**2)
+# timeit выдает результат 0.00243547599984594
+
+def eratosthenes_sieve(numb):
+    """
+    нахождения i-го по счёту простого числа
+    :param numb: Порядковый номер простого числа
+    :return: Возвращает число
+    """
+    count, start, end = 1, 3, 4 * numb
+
+    sieve = [i for i in range(start, end) if i % 2 != 0]
+    prime = [2]
+
+    if numb == 1:
+        return 2
+
+    while count < numb:
+        for i in range(len(sieve)):
+            if sieve[i] != 0:
+                count += 1
+
+                if count == numb:
+                    return sieve[i]
+
+                j = i + sieve[i]
+
+                while j < len(sieve):
+                    sieve[j] = 0
+                    j += sieve[i]
+
+        prime.extend([i for i in sieve if i != 0])
+
+        start, end = end, end + 2 * numb
+        sieve = [i for i in range(start, end) if i % 2 != 0]
+
+        for i in range(len(sieve)):
+            for num in prime:
+                if sieve[i] % num == 0:
+                    sieve[i] = 0
+                    break
+
+
+t1 = Timer("eratosthenes_sieve(5)", setup="from __main__ import eratosthenes_sieve")
+print('eratosthenes_sieve', t1.timeit(number=1000), 'milliseconds')
+
+# Сложность близка к O(n**3)
+# timeit выдает результат 0.005870107000191638
+
+
+# ВЫВОД:
+# Реализация нахождения i-го числа без решета Эротосфера
+# выглядит и эстетичнее и по производительности выигрывает.
+# Либо 3-й вариант - голова у меня не из того места растет :D
