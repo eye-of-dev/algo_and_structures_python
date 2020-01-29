2775f5139f4a264a4c0b19b46851e25a561513c6
Anton Lisikov <anton.lisikov@gmail.com>
2020-01-30 01:44:02 +0600

    feat: сделаны задания к седьмому уроку

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,53 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+from random import uniform
+
+
+def merge_sort(my_list):
+    """
+    Функция сортировки методом слияния
+    :param my_list: Исходный массив
+    :return: Выводит отсортированный результат
+    """
+
+    def merge(left, right):
+        res = []
+        i, j = 0, 0
+
+        while i < len(left) and j < len(right):
+
+            if left[i] < right[j]:
+                res.append(left[i])
+                i += 1
+            else:
+                res.append(right[j])
+                j += 1
+
+        res.extend(left[i:] if i < len(left) else right[j:])
+
+        return res
+
+    def div_half(my_list):
+
+        if len(my_list) == 1:
+            return my_list
+        elif len(my_list) == 2:
+            return my_list if my_list[0] <= my_list[1] else my_list[::-1]
+        else:
+            return merge(div_half(my_list[:len(my_list) // 2]),
+                         div_half(my_list[len(my_list) // 2:]))
+
+    return div_half(my_list)
+
+
+IN_LIST = [uniform(0.0, 50.0) for i in range(10)]
+
+print(IN_LIST)
+print(merge_sort(IN_LIST))
+
+# Результат
+# [40.66832066377046, 41.32023034368247, 45.18854582044614, 24.167202416647914 ....]
+# [1.6039689261568513, 8.913995798006274, 9.312809770630636, 10.449208819206568 ....]
+
--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,79 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+from random import randint
+from statistics import median
+
+
+def my_median(my_list):
+    """
+    Поиск медианы
+    :param my_list: Исходный массив
+    :return: Выводит медиану
+    """
+    len_list = len(my_list)
+    sum_list = sum(my_list)
+    avg = sum_list // len_list
+
+    one_list = []
+    two_list = []
+
+    for val in my_list:
+        if val < avg:
+            one_list.append(val)
+        else:
+            two_list.append(val)
+
+    # не исходный же массив)
+    one_list.sort(), two_list.sort()
+    one_len, two_len = len(one_list), len(two_list)
+
+    # Идея такая: разбить исходный массив на два "равных" массива
+    # тогда медиана будет, либо в первом массиве, последним элементом,
+    # либо во втором первым элементом
+
+    # код ниже извлекает значение медины из массива согластно
+    # логике выше. Но эта логика работает, если длины массивов различны
+    # на 1
+
+    # но т.к. длина одного из массива может быть больше длины
+    # другого на более чем 1, то нужны некоторые манипуляции(махинации)
+
+    if one_len > two_len:
+        index = one_len - two_len
+        if index > 1:
+            index = index // 2
+            one_list = one_list[:one_len - index]
+
+        one_list = one_list[::-1]
+        return one_list[index - 1]
+    else:
+        index = two_len - one_len
+        if index > 1:
+            index = index // 2
+            two_list = two_list[:two_len - index]
+            return two_list[index]
+
+        return two_list[index - 1]
+
+
+m = randint(1, 500)
+SIZE = 2 * m + 1
+
+IN_LIST = [randint(0, 500) for i in range(SIZE)]
+
+print(IN_LIST)
+print(my_median(IN_LIST))
+print(median(IN_LIST))
+
+
+# Результат
+# [136, 115, 299, 29, 439, 341, 189, 84, 108, 292, 315, 299, 489, 281, 362, 92 .... ]
+# 265
+# 265
+
+# P.S.
+# было сделано куча тестов, все они дали верный результат.
+# если по какой-то причение "говнокод" выше не работает, то пес с ним.
+# Медиана находится через сортировку исходного массива с извлечением
+# среднего элемента.
